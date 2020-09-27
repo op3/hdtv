op3/hdtv-unattached

Possibility to show fit residuals
The residuals of a fit can be displayed. Implementation is similar to
the fit decomposition. Residuals can be shown using the R/Minus + R
keyboard shortcuts or using the fit residuals show/hide commands.

hdtv/rootext/fit/EEFitter.cc
@@ -360,6 +360,9 @@ void EEFitter::_Fit(TH1 &hist) {
   // Create fit function
   fSumFunc = Util::make_unique<TF1>("f", this, &EEFitter::Eval, fMin, fMax,
                                     fNumParams, "EEFitter", "Eval");
+  
+  int b1 = hist.FindBin(fMin);
+  int b2 = hist.FindBin(fMax);
 
   // Init fit parameters
   // Note: this may set parameters several times, but that should not matter
@@ -396,6 +399,25 @@ void EEFitter::_Fit(TH1 &hist) {
 
   // Store Chi^2
   fChisquare = fSumFunc->GetChisquare();
+  
+  // Calculate Residuals
+  if (hist.GetXaxis()->GetXbins()->GetSize() != 0) {
+    fResiduals = Util::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetXaxis()->GetXbins()->GetArray());
+  } else {
+    fResiduals = Util::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetBinLowEdge(1),
+        hist.GetBinLowEdge(hist.GetNbinsX()+1));
+  }
+
+  for (int i = 0; i <= fResiduals->GetNbinsX(); ++i) {
+    if (i < b1 || b2 < i+1) {
+      fResiduals->SetBinContent(i, 0);
+    } else {
+      fResiduals->SetBinContent(i,
+         hist.GetBinContent(i) - fSumFunc->Eval(fResiduals->GetBinCenter(i)));
+    }
+  }
 
   // Finalize fitter
   fFinal = true;

hdtv/rootext/fit/TheuerkaufFitter.cc
@@ -551,6 +551,25 @@ void TheuerkaufFitter::_Fit(TH1 &hist) {
     fChisquare = fSumFunc->GetChisquare();
   }
 
+  // Calculate Residuals
+  if (hist.GetXaxis()->GetXbins()->GetSize() != 0) {
+    fResiduals = Util::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetXaxis()->GetXbins()->GetArray());
+  } else {
+    fResiduals = Util::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetBinLowEdge(1),
+        hist.GetBinLowEdge(hist.GetNbinsX()+1));
+  }
+
+  for (int i = 0; i <= fResiduals->GetNbinsX(); ++i) {
+    if (i < b1 || b2 < i+1) {
+      fResiduals->SetBinContent(i, 0);
+    } else {
+      fResiduals->SetBinContent(i,
+         hist.GetBinContent(i) - fSumFunc->Eval(fResiduals->GetBinCenter(i)));
+    }
+  }
+
   // Finalize fitter
   fFinal = true;
 }

op3/hdtv

hdtv/rootext/fit/EEFitter.cc
@@ -360,6 +360,9 @@ void EEFitter::_Fit(TH1 &hist) {
   // Create fit function
   fSumFunc = std::make_unique<TF1>("f", this, &EEFitter::Eval, fMin, fMax,
                                     fNumParams, "EEFitter", "Eval");
+  
+  int b1 = hist.FindBin(fMin);
+  int b2 = hist.FindBin(fMax);
 
   // Init fit parameters
   // Note: this may set parameters several times, but that should not matter
@@ -400,6 +403,25 @@ void EEFitter::_Fit(TH1 &hist) {
 
   // Store Chi^2
   fChisquare = fSumFunc->GetChisquare();
+  
+  // Calculate Residuals
+  if (hist.GetXaxis()->GetXbins()->GetSize() != 0) {
+    fResiduals = std::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetXaxis()->GetXbins()->GetArray());
+  } else {
+    fResiduals = std::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetBinLowEdge(1),
+        hist.GetBinLowEdge(hist.GetNbinsX()+1));
+  }
+
+  for (int i = 0; i <= fResiduals->GetNbinsX(); ++i) {
+    if (i < b1 || b2 < i+1) {
+      fResiduals->SetBinContent(i, 0);
+    } else {
+      fResiduals->SetBinContent(i,
+         hist.GetBinContent(i) - fSumFunc->Eval(fResiduals->GetBinCenter(i)));
+    }
+  }
 
   // Finalize fitter
   fFinal = true;

hdtv/rootext/fit/TheuerkaufFitter.cc
@@ -554,6 +554,25 @@ void TheuerkaufFitter::_Fit(TH1 &hist) {
     fChisquare = fSumFunc->GetChisquare();
   }
 
+  // Calculate Residuals
+  if (hist.GetXaxis()->GetXbins()->GetSize() != 0) {
+    fResiduals = std::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetXaxis()->GetXbins()->GetArray());
+  } else {
+    fResiduals = std::make_unique<TH1F>(
+        "", "", hist.GetNbinsX(), hist.GetBinLowEdge(1),
+        hist.GetBinLowEdge(hist.GetNbinsX()+1));
+  }
+
+  for (int i = 0; i <= fResiduals->GetNbinsX(); ++i) {
+    if (i < b1 || b2 < i+1) {
+      fResiduals->SetBinContent(i, 0);
+    } else {
+      fResiduals->SetBinContent(i,
+         hist.GetBinContent(i) - fSumFunc->Eval(fResiduals->GetBinCenter(i)));
+    }
+  }
+
   // Finalize fitter
   fFinal = true;
 }